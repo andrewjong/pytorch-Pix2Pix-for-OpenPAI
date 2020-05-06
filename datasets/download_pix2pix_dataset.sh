326fe2327c879eac88eadfd3fe1abbf2b4a881c4
Andrew Jong <andrewjong87@gmail.com>
2020-05-05 20:51:55 -0700

    make download quiet

--- datasets/download_pix2pix_dataset.sh
+++ datasets/download_pix2pix_dataset.sh
@@ -16,7 +16,7 @@ echo "Specified [$FILE]"
 URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/$FILE.tar.gz
 TAR_FILE=./datasets/$FILE.tar.gz
 TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $TAR_FILE
+wget -q -N $URL -O $TAR_FILE
 mkdir -p $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./datasets/
+tar -zxf $TAR_FILE -C ./datasets/
 rm $TAR_FILE
